9bc3024d023b04e09ce4685cc82be5e788aef476
Merve Cetinkaya <merve.cetinkaya@sap.com>
2018-10-30 14:00:54 +0100

    'trying to add patient view'

---
+++ webapp/controller/Patient.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+	"myhealthapp/controller/BaseController"
+], function(BaseController) {
+	"use strict";
+
+	return BaseController.extend("myhealthapp.controller.Patient", {
+
+	});
+
+});
